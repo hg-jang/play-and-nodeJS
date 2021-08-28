241ee94ec306316d2f9eda42b0602d8690b82844
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-08-29 00:19:25 +0900

    그룹 카드 연결

--- pages/myGroups.js
+++ pages/myGroups.js
@@ -1,20 +1,77 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { fbaseFirestore } from '../src/fbase'
 import Link from 'next/link'
-import classNames from 'classnames'
-import MyTeam from '../src/public/component/PublicMyTeam'
 import styles from '../src/css/myGroups.module.css'
+import classNames from 'classnames'
+import GroupCard from '../src/components/GroupCard'
 
 const myGroups = () => {
+  const uid = useSelector((state) => state.auth.currentUser?.uid)
+  const [groupList, setGroupList] = useState([])
+
+  const getTeamList = async () => {
+    try {
+      const joiningGroup = await fbaseFirestore.collection('whole users').doc(uid).collection('joining groups').get()
+
+      joiningGroup.forEach(async (group) => {
+        let groupObj = {}
+        
+        const docRef_1 = await fbaseFirestore.collection(group.data().groupName).doc('group information').get()
+        if(docRef_1.exists) {
+          groupObj = {
+            ...groupObj,
+            groupName: docRef_1.data().groupName,
+            groupIntroduce: docRef_1.data().groupIntroduce,
+            numberOfMember: docRef_1.data().numberOfMember,
+            createdDate: docRef_1.data().createdDate
+          }
+        }
+
+        const docRef_2 = await fbaseFirestore.collection(group.data().groupName).doc('group data').collection('admins').doc(uid).get()
+        if(docRef_2.exists) {
+          groupObj = {
+            ...groupObj,
+            isAdmin: true,
+          }
+        }
+
+        const docRef_3 = await fbaseFirestore.collection(group.data().groupName).doc('group data').collection('members').doc(uid).get()
+        if(docRef_3.exists) {
+          groupObj = {
+            ...groupObj,
+            joinedDate: Number(String(docRef_3.data().joinedDate).slice(0, 8))
+          }
+        }
+
+        setGroupList(prevGroupList => [...prevGroupList, groupObj])
+      })
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getTeamList()
+  }, [])
 
   return (
-    <>
-      {/* <MyTeam /> */}
-      <div className={styles.newTeam}>
-        <div className={classNames({["container"]: true, [styles.container__public_newTeam]: true})}>
-          <div className="button__index"><Link href="/public/public_createGroup"><a>그룹 생성하기</a></Link></div>
-          <div className="button__index"><Link href="/public/public_joinGroup"><a>그룹 가입하기</a></Link></div>
+    <div className={styles.my_groups}>
+      <div className={styles.joined_groups}>
+        <div className={classNames({["container"]: true, [styles.container__joined_groups]: true})}>
+          <h1>가입한 그룹</h1>
+          <div>
+            {groupList.map((group, index) => <GroupCard group={group} index={index} />)}
+          </div>
         </div>
       </div>
-    </>
+      <div className={styles.new_group}>
+        <div className={classNames({["container"]: true, [styles.container__new_group]: true})}>
+          <div className="button__index"><Link href="/createGroup"><a>그룹 생성하기</a></Link></div>
+          <div className="button__index"><Link href="/joinGroup"><a>그룹 가입하기</a></Link></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/GroupCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from 'next/link'
+import { Icon } from 'semantic-ui-react'
+import styles from '../css/myGroups.module.css'
+
+const GroupCard = ({ group, index }) => {
+  const defaultSrc = 'https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+
+  console.log('group card 호출 됨');
+  return (
+    <div className={styles.group_card} key={index}>
+      <div className={styles.group_image}>
+        <img src={defaultSrc} alt="team profile" />
+        {group.isAdmin === true ? <Link href={`/admin/${group.groupName}`}><a><Icon fitted className={styles.setting} name='setting' size='large' /></a></Link> : <></>}
+      </div>
+      <Link href={`/public/${group.group_name}`}><a><h1 className={styles.team_name}>{group.groupName}</h1></a></Link>
+      <h2 className={styles.group_introduce}>{group.groupIntroduce}</h2>
+      <ul className={styles.group_info}>
+        <li className={styles.created_date}>
+          <span className={styles.info_name}>그룹 생성일</span>
+          <span className={styles.info_content}>{group.createdDate}</span>
+        </li>
+        <li className={styles.joined_date}>
+          <span className={styles.info_name}>그룹 가입일</span>
+          <span className={styles.info_content}>{group.joinedDate}</span>
+        </li>
+        <li className={styles.member_number}>
+          <span className={styles.info_name}>그룹 멤버 수</span>
+          <span className={styles.info_content}>{group.numberOfMember}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default GroupCard
