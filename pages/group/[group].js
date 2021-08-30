8b965c1b645ebb14918d2992e477e6e543e9ea35
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-08-30 15:08:06 +0900

    그룹의 members 정보 가져오기 추가

--- pages/group/[group].js
+++ pages/group/[group].js
@@ -1,35 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
 import { fbaseFirestore } from '../../src/fbase'
-// import styles from '../../src/public/css/group_main.module.css'
-// import Nav from "../../src/public/component/Nav"
-// import Ranking from "../../src/public/component/Ranking"
-// import MemberList from "../../src/public/component/MemberList"
-// import Community from "../../src/public/component/Community"
-// import RecentGame from "../../src/public/component/RecentGame"
+// import Ranking from "../../src/components/Ranking"
+// import MemberList from "../../src/components/MemberList"
+// import Community from "../../src/components/Community"
+// import RecentGame from "../../src/components/RecentGame"
 // import Ad from "../../src/public/component/Ad"
-import { SET_GAMES } from '../../reducers/group'
+import styles from '../../src/css/group.module.css'
+import { SET_GAMES, SET_MEMBERS } from '../../reducers/group'
 
 const group_index = () => {
   const router = useRouter()
   const { group } = router.query
   
   const dispatch = useDispatch()
-  const { currentGroup } = useSelector((state) => state.group)
-
-  
-  // const groupName = group
-  // const [content, setContent] = useState('community')
-  // const [groupPlayers, setGroupPlayers] = useState([]);
-  // const [wholeGames, setWholeGames] = useState([]);
+  const { content, currentGroup } = useSelector((state) => state.group)
 
   const getGames = () => {
     let gamesArr = []
     fbaseFirestore.collection(group).doc('group data').collection('games').get()
     .then((games) => {
       games.forEach((game) => {
-        console.log('2 :', game.data());
         const gameObj = {
           winnerRatingAfter: game.data().winnerRatingAfter,
           loserRatingAfter: game.data().loserRatingAfter,
@@ -52,18 +44,33 @@ const group_index = () => {
     })
   }
 
-  // const getMembers = () => {
-  //   let membersArr = []
+  const getMembers = () => {
+    let membersArr = []
 
-  //   dbService.collection(group).doc('group data').collection('members').get()
-  //   .then((members) => {
-  //     members.forEach((member) => {
-  //       const memberObj = {
-
-  //       }
-  //     })
-  //   })
-  // }
+    fbaseFirestore.collection(group).doc('group data').collection('members').get()
+    .then((members) => {
+      members.forEach((member) => {
+        const memberObj = {
+          displayName: member.data().displayName,
+          photoURL: member.data().photoURL,
+          joinedDate: member.data().joinedDate,
+          rating: member.data().rating,
+          start_rating: member.data().start_rating,
+          allGames: member.data().allGames,
+          winnedGames: member.data().winnedGames,
+          losedGames: member.data().losedGames,
+          status: member.data().status,
+        }
+        membersArr = membersArr.concat(memberObj)
+      })
+    })
+    .then(() => {
+      dispatch({
+        type: SET_MEMBERS,
+        data: membersArr,
+      })
+    })
+  }
 
 
   useEffect(() => {
@@ -74,29 +81,27 @@ const group_index = () => {
   }, [router])
 
   useEffect(() => {
-    console.log('페이지 : ', currentGroup);
+    console.log('가져온 그룹 정보 : ', currentGroup);
   }, [currentGroup])
 
   return (
-    <p>그룹 메인</p>
-    // <div className={styles.publicContainer}>
-    //   <Nav setContent={setContent} />
-    //   <div className={styles.teamContainer}>
-    //     {content === 'ranking' && <Ranking groupPlayers={groupPlayers} />}
-    //     {content === 'member list' && <MemberList groupName={groupName} groupPlayers={groupPlayers} />}
-    //     {content === 'community' && <Community groupName={groupName}/>}
-    //   </div>
-    //   {content !== 'community' &&
-    //   <div className={styles.aside}>
-    //     <div className={styles.aside1}>
-    //       <RecentGame wholeGames={wholeGames} />
-    //     </div>
-    //     <div className={styles.aside2}>
-    //       <Ad />
-    //     </div>
-    //   </div>
-    //   }
-    // </div>
+    <>
+      {/* <div className={styles.contents}>
+        {content === 'community' && <Community />}
+        {content === 'ranking' && <Ranking />}
+        {content === 'member list' && <MemberList />}
+      </div>
+      {content !== 'community' &&
+      <div className={styles.asides}>
+        <div className={styles.aside1}>
+          <RecentGame wholeGames={wholeGames} />
+        </div>
+        <div className={styles.aside2}>
+          <Ad />
+        </div>
+      </div>
+      } */}
+    </>
   )
 }
 
--- reducers/group.js
+++ reducers/group.js
@@ -1,13 +1,18 @@
 export const initialState = {
-  isGamesLoading: false,
-  isGamesLoaded: false,
-  loadGamesError: null,
+  content: 'community',
   currentGroup: {},
 }
 
 export const SET_GAMES = 'SET_GAMES'
 export const SET_MEMBERS = 'SET_MEMBERS'
+export const SET_CONTENT = 'SET_CONTENT'
 
+export const onClickSetContent = (e) => {
+  return {
+    type: SET_CONTENT,
+    data: e.target.dataset.content,
+  }
+}
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
@@ -17,7 +22,20 @@ const reducer = (state = initialState, action) => {
         currentGroup: {
           ...state.currentGroup,
           games: action.data,
-        } 
+        }
+      }
+    case SET_MEMBERS:
+      return {
+        ...state,
+        currentGroup: {
+          ...state.currentGroup,
+          members: action.data,
+        }
+      }
+    case SET_CONTENT:
+      return {
+        ...state,
+        content: action.data,
       }
     default:
       return state
